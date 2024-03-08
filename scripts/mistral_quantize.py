8aef36c6d1ae94aaaa0a35c6d3550bf90e4f56a2
Leslie Dees <leslie.dees@duke.edu>
2024-03-07 22:16:40 -0500

    Attempt new quantization method

--- scripts/mistral_quantize.py
+++ scripts/mistral_quantize.py
@@ -1,5 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from transformers import BitsAndBytesConfig
+from torch import bfloat16
 
 def load_model_quantized(model_id, quantized = True):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -9,21 +11,34 @@ def load_model_quantized(model_id, quantized = True):
 
 def main():
     model_id = "mistralai/Mistral-7B-Instruct-v0.2"
-    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Prompt user for quantized model usage
-    quantized_input = input("Do you want to use the quantized version of the model? (yes/no): ").strip().lower()
-    if quantized_input in {"yes", "y"}:
-        quantized = True
-    elif quantized_input in {"no", "n"}:
-        quantized = False
-    else:
-        print("Invalid input. Please type 'yes' or 'no'.")
-
-    model, device  = load_model_quantized(model_id, quantized)
+#     quantized_input = input("Do you want to use the quantized version of the model? (yes/no): ").strip().lower()
+#     if quantized_input in {"yes", "y"}:
+#         quantized = True
+#     elif quantized_input in {"no", "n"}:
+#         quantized = False
+#     else:
+#         print("Invalid input. Please type 'yes' or 'no'.")
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True, #4-bit quantization
+        bnb_4bit_quant_type='nf4', #Normalized float 4
+        bnb_4bit_use_double_quant=True, #Second quantization after the first
+        bnb_4bit_compute_dtype=bfloat16 #Computation type
+    )
+    
     tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        quantization_config=bnb_config,
+        device_map='auto'
+    )
+    
     if tokenizer.pad_token is None:
             tokenizer.pad_token = tokenizer.eos_token
-
+            
+    pipe = pipeline(model=model, tokenizer=tokenizer, task='text-generation')
+    
     while True:
         # User input
         user_input = input("You: ")
@@ -31,17 +46,13 @@ def main():
             print("Exiting...")
             break
 
-        messages = [
-            {"role": "user", "content": user_input}
-        ]
-
-        encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
-
-        model_inputs = encodeds.to(device)
-        
-        generated_ids = model.generate(model_inputs, pad_token_id=tokenizer.pad_token_id, max_new_tokens = 1000, do_sample=True)
-        decoded = tokenizer.batch_decode(generated_ids)
-        print("Model: ", decoded[0])
-
+        outputs = pipe(
+            user_input,
+            max_new_tokens=1024,
+            do_sample=True,
+            temperature=0.1,
+            top_p=0.95
+        )
+        print("Model: ", outputs[0]["generated_text"])
 if __name__ == "__main__":
     main()
