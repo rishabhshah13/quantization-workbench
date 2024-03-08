79de1e4c9b5062921ee533cd22faabe9137ca95c
Leslie Dees <leslie.dees@duke.edu>
2024-03-07 22:19:52 -0500

    Import

--- scripts/mistral_quantize.py
+++ scripts/mistral_quantize.py
@@ -1,4 +1,4 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import torch
 from transformers import BitsAndBytesConfig
 from torch import bfloat16
